a8cf834a342e55afbf546e3fdff6e53e213f3de8
sanjip mahat <146052754+sanjip1203@users.noreply.github.com>
2025-03-06 08:30:36 +0545

    setup react and it's layout in frontend

---
+++ movie-rater/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> Movie _rater</h1>
+      <div className='grid grid-cols-2'>
+      <h1> Movie list</h1>
+      <h1>Movie Details</h1>
+    
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
